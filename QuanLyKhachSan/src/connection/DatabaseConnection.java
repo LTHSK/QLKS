28ba703b465823c6139c142d5542bf06ada316ff
khanh260802 <casiofx580@gmail.com>
2023-05-18 23:36:19 +0700

    nothing change hehe

--- QuanLyKhachSan/src/connection/DatabaseConnection.java
+++ QuanLyKhachSan/src/connection/DatabaseConnection.java
@@ -1,22 +1,22 @@
-package connection;
+package connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 //Huy 
 public class DatabaseConnection {
-    private static DatabaseConnection instance = new DatabaseConnection();
+    private static DatabaseConnection instance = new DatabaseConnection();
     public static DatabaseConnection getInstance()
     {
-        return instance;
+        return instance;
     }
     
     public static Connection opConnection() throws ClassNotFoundException, SQLException{
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String connectionUrl = "jdbc:sqlserver://localhost;database=QLKS"; 
-        String user = "sa";
-        String pass = "123456";
-        Connection con = DriverManager.getConnection(connectionUrl,user,pass);
-        return con;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String connectionUrl = "jdbc:sqlserver://localhost;database=QLKS"; 
+        String user = "sa";
+        String pass = "123456";
+        Connection con = DriverManager.getConnection(connectionUrl,user,pass);
+        return con;
     }
 }
